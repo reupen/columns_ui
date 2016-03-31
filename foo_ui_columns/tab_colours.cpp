ccadf974112469a48bee9773cfd6255bc2faab75
Reupen Shah <reupen@users.noreply.github.com>
2016-03-31 21:25:11 +0100

    partially fix active item frame colour selection in prefs

--- foo_ui_columns/tab_colours.cpp
+++ foo_ui_columns/tab_colours.cpp
@@ -218,10 +218,20 @@ void tab_appearance::update_buttons()
 
 bool tab_appearance::get_colour_patch_enabled(cui::colours::colour_identifier_t p_identifier)
 {
-	if (cui::colours::colour_active_item_frame == p_identifier)
-		return cui::colours::helper(m_element_guid).get_bool(cui::colours::bool_use_custom_active_item_frame);
-	else
-		return (!m_element_api.is_valid() || (m_element_api->get_supported_colours() & (1 << p_identifier)));
+	cui::colours::helper colour_helper(m_element_guid);
+
+	if (cui::colours::colour_active_item_frame == p_identifier) {
+		if (!m_element_api.is_valid()) {
+			return colour_helper.get_bool(cui::colours::bool_use_custom_active_item_frame);
+		} else {
+			bool is_colour_supported = (m_element_api->get_supported_colours() & (1 << p_identifier)) != 0;
+			bool use_custom_frame_supported = (m_element_api->get_supported_bools() & (1 << cui::colours::bool_flag_use_custom_active_item_frame)) != 0;
+			
+			return !is_colour_supported || !use_custom_frame_supported || colour_helper.get_bool(cui::colours::bool_use_custom_active_item_frame);
+		}
+	} else {
+		return !m_element_api.is_valid() || (m_element_api->get_supported_colours() & (1 << p_identifier));
+	}
 }
 
 bool tab_appearance::get_change_colour_enabled(cui::colours::colour_identifier_t p_identifier)
