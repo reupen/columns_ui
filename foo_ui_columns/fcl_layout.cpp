98c924f80cc2cae5710e4bcdc0f6f0b6574b1bec
Reupen Shah <reupen@users.noreply.github.com>
2017-05-14 19:53:23 +0100

    [#53] Use fcl::reader::read_raw_item()
    Use fcl::reader::read_raw_item() to read settings::allow_locked_panel_resizing instead of the removed fcl::reader::read_item_indirect().

--- foo_ui_columns/fcl_layout.cpp
+++ foo_ui_columns/fcl_layout.cpp
@@ -157,7 +157,7 @@ class export_layout_misc : public cui::fcl::dataset
 				reader.read_item(settings::show_status_pane);
 				break;
 			case identifier_allow_locked_panel_resizing:
-				reader.read_item_indirect<bool>(settings::allow_locked_panel_resizing);
+				settings::allow_locked_panel_resizing = reader.read_raw_item<bool>();
 				break;
 			default:
 				reader.skip(element_size);
