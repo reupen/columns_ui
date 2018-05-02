d889b56e536298f87ac5bf6e6f0a144e056b573f
Reupen Shah <reupen@users.noreply.github.com>
2018-05-02 20:01:34 +0100

    Rename IDD_FILTER_BEHAVIOUR to IDD_PREFS_FILTER_BEHAVIOUR

--- foo_ui_columns/config_filter.cpp
+++ foo_ui_columns/config_filter.cpp
@@ -385,7 +385,7 @@ public:
     }
     HWND create(HWND wnd) override
     {
-        return m_helper.create(wnd, IDD_FILTER_BEHAVIOUR,
+        return m_helper.create(wnd, IDD_PREFS_FILTER_BEHAVIOUR,
             [this](auto&&... args) { return on_message(std::forward<decltype(args)>(args)...); });
     }
     const char* get_name() override { return "Behaviour"; }
