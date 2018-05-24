7a2bf798a3ce5559c3aa00e0181debe07985e15b
Reupen Shah <reupen@users.noreply.github.com>
2018-05-24 20:38:34 +0100

    Use online help for style script and global variables help commands

---
+++ foo_ui_columns/urls.h
@@ -0,0 +1,6 @@
+#pragma once
+
+namespace cui::urls {
+constexpr std::wstring_view colour_script_help = L"https://wiki.yuo.be/columns_ui:config:colour_string";
+constexpr std::wstring_view global_variables_help = L"https://wiki.yuo.be/columns_ui:config:global_variables";
+} // namespace cui::urls
