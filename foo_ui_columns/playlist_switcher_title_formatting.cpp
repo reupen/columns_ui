efda5d41d7f0a28c6464e463cd95e4e2a99b9c5b
Reupen Shah <reupen@users.noreply.github.com>
2019-11-02 19:30:28 +0000

    Refactor playlist switcher title formatting code
    This refactors the playlist switcher title formatting code using more generic and less repetitive techniques.
    
    This is also to make it easier to add new title formatting fields.

---
+++ foo_ui_columns/playlist_switcher_title_formatting.cpp
@@ -0,0 +1,96 @@
+#include "stdafx.h"
+#include "playlist_switcher_title_formatting.h"
+#include "title_formatting.h"
+
+namespace cui {
+
+namespace {
+
+class LazyFieldCalculator {
+public:
+    LazyFieldCalculator(size_t playlist_index) : m_playlist_index(playlist_index) {}
+
+    t_filesize total_file_size()
+    {
+        if (!m_total_file_size)
+            m_total_file_size = metadb_handle_list_helper::calc_total_size(tracks(), true);
+
+        return *m_total_file_size;
+    }
+
+    double total_duration()
+    {
+        if (!m_total_duration)
+            m_total_duration = tracks().calc_total_duration();
+
+        return *m_total_duration;
+    }
+
+private:
+    const metadb_handle_list_t<pfc::alloc_fast_aggressive>& tracks()
+    {
+        if (!m_tracks) {
+            m_tracks = metadb_handle_list_t<pfc::alloc_fast_aggressive>{};
+            const auto size = m_playlist_api->playlist_get_item_count(m_playlist_index);
+            m_tracks->prealloc(size);
+            m_playlist_api->playlist_get_all_items(m_playlist_index, *m_tracks);
+        }
+        return *m_tracks;
+    }
+
+    size_t m_playlist_index{};
+    std::optional<double> m_total_duration;
+    std::optional<t_filesize> m_total_file_size;
+    std::optional<metadb_handle_list_t<pfc::alloc_fast_aggressive>> m_tracks;
+    static_api_ptr_t<playlist_manager_v3> m_playlist_api;
+};
+
+} // namespace
+
+pfc::string8 format_playlist_title(unsigned index)
+{
+    auto playlist_api = playlist_manager_v3::get();
+    auto playback_api = playback_control::get();
+
+    pfc::string8 name;
+    playlist_api->playlist_get_name(index, name);
+
+    if (!cfg_playlist_switcher_use_tagz)
+        return name;
+
+    auto size = playlist_api->playlist_get_item_count(index);
+    auto is_locked = playlist_api->playlist_lock_is_present(index);
+    auto is_active = playlist_api->get_active_playlist() == index;
+    auto is_playing = playback_api->is_playing() && playlist_api->get_playing_playlist() == index;
+
+    LazyFieldCalculator lazy_fields{index};
+
+    auto file_size_getter
+        = [&lazy_fields]() { return std::string(mmh::FileSizeFormatter(lazy_fields.total_file_size())); };
+
+    auto file_size_raw_getter
+        = [&lazy_fields]() { return std::string(pfc::format_uint(lazy_fields.total_file_size())); };
+
+    auto length_getter = [&lazy_fields]() { return std::string(pfc::format_time_ex(lazy_fields.total_duration(), 0)); };
+
+    tf::FieldProviderTitleformatHook::FieldMap field_map{{"title", name}, {"size", std::string(pfc::format_uint(size))},
+        {"is_locked", is_locked}, {"is_active", is_active}, {"is_playing", is_playing}, {"filesize", file_size_getter},
+        {"filesize_raw", file_size_raw_getter}, {"length", length_getter}};
+
+    if (is_locked) {
+        pfc::string8 lock_name;
+        playlist_api->playlist_lock_query_name(index, lock_name);
+        field_map["lock_name"sv] = std::string(lock_name);
+    }
+
+    tf::FieldProviderTitleformatHook field_provider_hook(field_map);
+
+    pfc::string8 title;
+    service_ptr_t<titleformat_object> to_temp;
+    titleformat_compiler::get()->compile_safe(to_temp, cfg_playlist_switcher_tagz);
+
+    to_temp->run(&field_provider_hook, title, nullptr);
+
+    return title;
+}
+} // namespace cui
---
+++ foo_ui_columns/title_formatting.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "../foobar2000/SDK/foobar2000.h"
+
+namespace cui::tf {
+
+namespace internal {
+
+struct CaseInsensitiveUtf8Comparator {
+    bool operator()(const std::string_view& left, const std::string_view& right) const
+    {
+        return stricmp_utf8_ex(left.data(), left.size(), right.data(), right.size()) < 0;
+    }
+};
+
+// See https://wg21.link/P0608R3
+class ExplicitBool {
+public:
+    template <class T, std::enable_if_t<std::is_same_v<bool, std::decay_t<T>>>* = nullptr>
+    ExplicitBool(T value) : m_value{value}
+    {
+    }
+
+    operator bool() const { return m_value; }
+
+private:
+    bool m_value{};
+};
+
+} // namespace internal
+
+class FieldProviderTitleformatHook : public titleformat_hook {
+public:
+    using FieldValue = std::variant<std::string, std::string_view, pfc::string8, internal::ExplicitBool,
+        std::function<std::string()>>;
+    using FieldMap = std::map<std::string_view, FieldValue, internal::CaseInsensitiveUtf8Comparator>;
+
+    FieldProviderTitleformatHook(FieldMap field_map) : m_field_map(std::move(field_map)){};
+
+    bool process_field(
+        titleformat_text_out* p_out, const char* p_name, t_size p_name_length, bool& p_found_flag) override;
+
+    bool process_function(titleformat_text_out* p_out, const char* p_name, t_size p_name_length,
+        titleformat_hook_function_params* p_params, bool& p_found_flag) override
+    {
+        return false;
+    }
+
+    FieldMap m_field_map;
+};
+
+} // namespace cui::tf
