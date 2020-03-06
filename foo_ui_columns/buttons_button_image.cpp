reupen/columns_ui

commit 09be8e50007a49b24b579cc6885d8c3d427a317b

Use the Windows Imaging Component to load images
This resurrects the Windows Imaging Component-based image loader, and uses it in the buttons toolbar, artwork panel and playlist view.

This brings support for additional image formats, such as WebP on Windows 10 1809 and newer.

This also adds the Windows Implementation Library as a dependency, and sets _WIN32_WINNT to 0x601 to allow the use of Win32 API functions that require Windows 7.

foo_ui_columns/buttons_button_image.cpp
@@ -1,5 +1,6 @@
 #include "stdafx.h"
 #include "buttons.h"
+#include "wic.h"
 
 ButtonsToolbar::ButtonImage::~ButtonImage()
 {
@@ -14,8 +15,11 @@ bool ButtonsToolbar::ButtonImage::is_valid()
 {
     return m_bm != nullptr;
 }
+
 void ButtonsToolbar::ButtonImage::load(const Button::CustomImage& p_image)
 {
+    TRACK_CALL_TEXT("cui::ButtonsToolbar::ButtonImage::load");
+
     m_mask_type = p_image.m_mask_type;
     m_mask_colour = p_image.m_mask_colour;
 
@@ -29,9 +33,12 @@ void ButtonsToolbar::ButtonImage::load(const Button::CustomImage& p_image)
         m_icon = (HICON)uLoadImage(core_api::get_my_instance(), fullPath, IMAGE_ICON, GetSystemMetrics(SM_CXSMICON),
             GetSystemMetrics(SM_CYSMICON), LR_LOADFROMFILE);
     else {
-        m_bm = g_load_png_gdiplus(nullptr, fullPath);
-        // g_load_png_wic(0, p_image.m_path);
-        // read_png(0, p_image.m_path);
+        try {
+            m_bm = cui::wic::create_hbitmap_from_path(p_image.m_path).release();
+        } catch (const std::exception& ex) {
+            fbh::print_to_console(u8"Buttons toolbar – loading image failed: ", ex.what());
+            m_bm = nullptr;
+        }
     }
     if (m_bm) {
         switch (p_image.m_mask_type) {

azure/pipeline.yml
@@ -1,7 +1,7 @@
 variables:
   solution: vc16/columns_ui-public.sln
   llvmMsBuildArgs: /p:PlatformToolset=ClangCL;UseLldLink=True;VcpkgAutoLink=False;WholeProgramOptimization=False;SpectreMitigation=
-  vcpkgInstallArgs: 'ms-gsl range-v3 --overlay-ports=$(System.DefaultWorkingDirectory)\ports'
+  vcpkgInstallArgs: 'ms-gsl range-v3 wil --overlay-ports=$(System.DefaultWorkingDirectory)\ports'
 jobs:
 - template: job-build.yml
   parameters:

foo_ui_columns/artwork.cpp
@@ -2,6 +2,7 @@
 
 #include "artwork.h"
 #include "config.h"
+#include "wic.h"
 
 namespace artwork_panel {
 // {A24038C7-C055-45ed-B631-CC8FD2A22473}
@@ -166,7 +167,7 @@ LRESULT ArtworkPanel::on_message(HWND wnd, UINT msg, WPARAM wp, LPARAM lp)
         static_api_ptr_t<playlist_manager_v3>()->unregister_callback(this);
         static_api_ptr_t<play_callback_manager>()->unregister_callback(this);
         m_selection_handles.remove_all();
-        m_image.release();
+        m_image.reset();
         m_bitmap.release();
         if (m_gdiplus_initialised)
             Gdiplus::GdiplusShutdown(m_gdiplus_instance);
@@ -392,7 +393,7 @@ void ArtworkPanel::show_emptycover()
             pfc::com_ptr_t<mmh::IStreamMemblock> pStream
                 = new mmh::IStreamMemblock((const t_uint8*)data->get_ptr(), data->get_size());
             {
-                m_image = pfc::rcnew_t<Gdiplus::Bitmap>(pStream.get_ptr());
+                m_image = std::make_unique<Gdiplus::Bitmap>(pStream.get_ptr());
                 pStream.release();
                 if (m_image->GetLastStatus() == Gdiplus::Ok) {
                     flush_cached_bitmap();
@@ -408,42 +409,49 @@ void ArtworkPanel::show_emptycover()
 
 bool ArtworkPanel::refresh_image(t_size index)
 {
-    if (m_artwork_loader.is_valid() && m_artwork_loader->IsReady()) {
-        album_art_data_ptr data;
-        if (m_artwork_loader->Query(g_artwork_types[index], data))
-        // if (m_nowplaying_artwork_loader.get_data(m_position, data))
-        {
-            pfc::com_ptr_t<mmh::IStreamMemblock> pStream
-                = new mmh::IStreamMemblock((const t_uint8*)data->get_ptr(), data->get_size());
-            /// m_image.release();
-            /// flush_cached_bitmap();
-            {
-                m_image = pfc::rcnew_t<Gdiplus::Bitmap>(pStream.get_ptr());
-                pStream.release();
-                if (m_image->GetLastStatus() == Gdiplus::Ok) {
-                    flush_cached_bitmap();
-                    RedrawWindow(get_wnd(), nullptr, nullptr, RDW_INVALIDATE | RDW_INVALIDATE);
-                    return true;
-                }
-            }
-        }
+    TRACK_CALL_TEXT("cui::ArtworkPanel::refresh_image");
+
+    flush_image();
+
+    if (!m_artwork_loader.is_valid() || !m_artwork_loader->IsReady())
+        return false;
+
+    album_art_data_ptr data;
+    if (!m_artwork_loader->Query(g_artwork_types[index], data))
+        return false;
+
+    cui::wic::BitmapData bitmap_data{};
+    try {
+        bitmap_data = cui::wic::decode_image_data(data->get_ptr(), data->get_size());
+    } catch (const std::exception& ex) {
+        fbh::print_to_console(u8"Artwork panel – loading image failed: ", ex.what());
+        return false;
     }
-    return false;
+
+    m_image = cui::gdip::create_bitmap_from_32bpp_data(
+        bitmap_data.width, bitmap_data.height, bitmap_data.stride, bitmap_data.data.data(), bitmap_data.data.size());
+
+    if (!m_image)
+        return false;
+
+    RedrawWindow(get_wnd(), nullptr, nullptr, RDW_INVALIDATE);
+    return true;
 }
+
 void ArtworkPanel::flush_cached_bitmap()
 {
     m_bitmap.release();
 }
 void ArtworkPanel::flush_image()
 {
-    m_image.release();
+    m_image.reset();
     flush_cached_bitmap();
 }
 void ArtworkPanel::refresh_cached_bitmap()
 {
     RECT rc;
     GetClientRect(get_wnd(), &rc);
-    if (RECT_CX(rc) && RECT_CY(rc) && m_image.is_valid()) {
+    if (RECT_CX(rc) && RECT_CY(rc) && m_image) {
         HDC dc = nullptr;
         HDC dcc = nullptr;
         dc = GetDC(get_wnd());

foo_ui_columns/artwork.h
@@ -185,7 +185,7 @@ private:
     // pfc::rcptr_t<CCustomAlbumArtLoader> m_artwork_loader;
     pfc::refcounted_object_ptr_t<ArtworkReaderManager> m_artwork_loader;
     // now_playing_album_art_manager m_nowplaying_artwork_loader;
-    pfc::rcptr_t<Gdiplus::Bitmap> m_image;
+    std::unique_ptr<Gdiplus::Bitmap> m_image;
     gdi_object_t<HBITMAP>::ptr_t m_bitmap;
     t_size m_position{0};
     t_size m_track_mode;

foo_ui_columns/artwork_helpers.cpp
@@ -281,7 +281,7 @@ unsigned artwork_panel::ArtworkReader::read_artwork(abort_callback& p_abort)
                 for (t_size i = 0; i < count && !b_found; i++) {
                     pfc::string8 path;
                     if (m_handle->format_title_legacy(nullptr, path, to[i], nullptr)) {
-                        const char* image_extensions[] = {"jpg", "jpeg", "gif", "bmp", "png"};
+                        const char* image_extensions[] = {"bmp", "gif", "jpeg", "jpg", "png", "webp"};
 
                         t_size i, count = tabsize(image_extensions);
 

foo_ui_columns/buttons.cpp
@@ -654,10 +654,11 @@ BOOL CALLBACK ButtonsToolbar::ConfigChildProc(HWND wnd, UINT msg, WPARAM wp, LPA
                     || (uGetFileAttributes(temp) & FILE_ATTRIBUTE_DIRECTORY))
                     temp.reset();
 
-                if (uGetOpenFileName(wnd,
-                        "Image Files (*.bmp;*.png;*.gif;*.tiff;*.ico)|*.bmp;*.png;*.gif;*.tiff;*.ico|All Files "
-                        "(*.*)|*.*",
-                        0, "png", "Choose image", nullptr, temp, FALSE)) {
+                constexpr auto extension_mask
+                    = "Image Files (*.bmp;*.gif;*.ico;*.png;*.tiff;*.webp)|*.bmp;*.gif;*.ico;*.png;*.tiff;*.webp|All "
+                      "Files (*.*)|*.*";
+
+                if (uGetOpenFileName(wnd, extension_mask, 0, "png", "Choose image", nullptr, temp, FALSE)) {
                     ptr->m_image->m_path = temp;
                     uSendDlgItemMessageText(
                         wnd, IDC_IMAGE_PATH, WM_SETTEXT, 0, (true) ? ptr->m_image->m_path.get_ptr() : "");

foo_ui_columns/foo_ui_columns.vcxproj
@@ -100,7 +100,7 @@
       </HeaderFileName>
     </Midl>
     <ClCompile>
-      <PreprocessorDefinitions>WIN32;NDEBUG;_WINDOWS;_USRDLL;_WIN32_WINNT=0x0600;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>WIN32;_WIN32_WINNT=0x0601;NDEBUG;_WINDOWS;_USRDLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <RuntimeTypeInfo>false</RuntimeTypeInfo>
       <PrecompiledHeader>Use</PrecompiledHeader>
       <PrecompiledHeaderFile>stdafx.h</PrecompiledHeaderFile>
@@ -147,7 +147,7 @@
       </HeaderFileName>
     </Midl>
     <ClCompile>
-      <PreprocessorDefinitions>WIN32;NDEBUG;_WINDOWS;_USRDLL;_WIN32_WINNT=0x0600;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>WIN32;_WIN32_WINNT=0x0601;NDEBUG;_WINDOWS;_USRDLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <RuntimeTypeInfo>false</RuntimeTypeInfo>
       <PrecompiledHeader>Use</PrecompiledHeader>
       <PrecompiledHeaderFile>stdafx.h</PrecompiledHeaderFile>
@@ -197,7 +197,7 @@
     <ClCompile>
       <AdditionalOptions>/Zc:threadSafeInit- -experimental:preprocessor /Zm125 /source-charset:utf-8 %(AdditionalOptions)</AdditionalOptions>
       <Optimization>Disabled</Optimization>
-      <PreprocessorDefinitions>WIN32;_DEBUG;_WINDOWS;_USRDLL;_WIN32_WINNT=0x600;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>WIN32;_WIN32_WINNT=0x0601;_DEBUG;_WINDOWS;_USRDLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <BasicRuntimeChecks>EnableFastChecks</BasicRuntimeChecks>
       <RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>
       <PrecompiledHeader>Use</PrecompiledHeader>
@@ -243,7 +243,7 @@
     <ClCompile>
       <AdditionalOptions>/Zc:threadSafeInit- -experimental:preprocessor /Zm125 /source-charset:utf-8 %(AdditionalOptions)</AdditionalOptions>
       <Optimization>Disabled</Optimization>
-      <PreprocessorDefinitions>WIN32;_DEBUG;_WINDOWS;_USRDLL;_WIN32_WINNT=0x600;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>WIN32;_WIN32_WINNT=0x0601;_DEBUG;_WINDOWS;_USRDLL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <BasicRuntimeChecks>EnableFastChecks</BasicRuntimeChecks>
       <RuntimeLibrary>MultiThreadedDebug</RuntimeLibrary>
       <PrecompiledHeader>Use</PrecompiledHeader>
@@ -477,6 +477,7 @@
     <ClCompile Include="tab_layout.cpp" />
     <ClCompile Include="tab_layout_misc.cpp" />
     <ClCompile Include="tab_pview_artwork.cpp" />
+    <ClCompile Include="wic.cpp" />
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include=".\artwork_helpers.h" />
@@ -551,6 +552,7 @@
     <ClInclude Include="tab_layout_misc.h" />
     <ClInclude Include="title_formatting.h" />
     <ClInclude Include="urls.h" />
+    <ClInclude Include="wic.h" />
   </ItemGroup>
   <ItemGroup>
     <None Include=".clang-format" />

foo_ui_columns/foo_ui_columns.vcxproj.filters
@@ -512,6 +512,9 @@
     <ClCompile Include="playlist_switcher_title_formatting.cpp">
       <Filter>Playlist switcher</Filter>
     </ClCompile>
+    <ClCompile Include="wic.cpp">
+      <Filter>Utilities</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include=".\resource.h">
@@ -727,6 +730,9 @@
     </ClInclude>
     <ClInclude Include="playlist_switcher_title_formatting.h">
       <Filter>Playlist switcher</Filter>
+    </ClInclude>
+    <ClInclude Include="wic.h">
+      <Filter>Utilities</Filter>
     </ClInclude>
   </ItemGroup>
   <ItemGroup>

commit 6ca3f173eed5fe0260a346952e07fe35212834fa

Correct loading of images with relative paths in the buttons toolbar
This fixes a regression in 09be8e50007a49b24b579cc6885d8c3d427a317b where relative paths of custom images in the buttons toolbar were not resolved.

This may have caused them to fail to load if the current working directory wasn't the one expected.

This only affected image file extensions other than `bmp` and `ico`.

foo_ui_columns/buttons_button_image.cpp
@@ -34,7 +34,7 @@ void ButtonsToolbar::ButtonImage::load(const Button::CustomImage& p_image)
             GetSystemMetrics(SM_CYSMICON), LR_LOADFROMFILE);
     else {
         try {
-            m_bm = cui::wic::create_hbitmap_from_path(p_image.m_path).release();
+            m_bm = cui::wic::create_hbitmap_from_path(fullPath).release();
         } catch (const std::exception& ex) {
             fbh::print_to_console(u8"Buttons toolbar – loading image failed: ", ex.what());
             m_bm = nullptr;