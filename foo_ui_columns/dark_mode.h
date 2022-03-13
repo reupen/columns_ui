974b1d7cae6a6ba78b5caef9f4e932ca610f7798
Reupen Shah <reupen@users.noreply.github.com>
2022-03-13 21:11:36 +0000

    Tweak dark mode background colours
    This:
    
    - standardises on a single light background colour used in e.g. the toolbars
    - adjusts the dark background colour on Windows 11 to be consistent with its File Explorer
    - corrects the minimum Windows 10 build check for dark mode

--- foo_ui_columns/dark_mode.h
+++ foo_ui_columns/dark_mode.h
@@ -11,6 +11,7 @@ enum class ColourID {
     LayoutBackground,
     PanelCaptionText,
     PanelCaptionBackground,
+    RebarBackground,
     RebarBandBorder,
     StatusBarBackground,
     StatusBarText,
