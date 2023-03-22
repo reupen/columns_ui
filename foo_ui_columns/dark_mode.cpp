83042c01410a2d49d1e80574bd0b2cbcf879bdf2
marc2k3 <marc2003@live.co.uk>
2023-03-22 12:35:52 +0000

    Update max Windows 11 version for dark mode menus
    Ref: https://hydrogenaud.io/index.php/topic,28647.msg1023999.html#msg1023999

--- foo_ui_columns/dark_mode.cpp
+++ foo_ui_columns/dark_mode.cpp
@@ -53,7 +53,7 @@ bool are_private_apis_allowed()
     if (osvi.dwMajorVersion != 10 || osvi.dwMinorVersion != 0)
         return false;
 
-    return osvi.dwBuildNumber >= 19041 && osvi.dwBuildNumber <= 22623;
+    return osvi.dwBuildNumber >= 19041 && osvi.dwBuildNumber <= 22624;
 }
 
 void set_app_mode(PreferredAppMode mode)
