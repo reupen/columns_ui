d808dab0ff49fb07b3781efe08fd17a9a11f6429
Reupen Shah <reupen@users.noreply.github.com>
2025-08-04 13:17:18 +0100

    Move preferences layout tab context menu handler to separate method

--- foo_ui_columns/tab_layout.h
+++ foo_ui_columns/tab_layout.h
@@ -94,6 +94,7 @@ private:
     void switch_to_preset(HWND wnd, size_t index);
 
     INT_PTR on_message(HWND wnd, UINT msg, WPARAM wp, LPARAM lp);
+    bool handle_wm_contextmenu(HWND wnd, HWND contextmenu_wnd, POINT pt);
     void on_tree_selection_change(HTREEITEM tree_item);
 
     HWND m_wnd_tree{};
