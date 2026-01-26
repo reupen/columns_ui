reupen/columns_ui

commit 24edec9448d22f3eb486d38a781d2d5acd8ac7e7

Add use hardware acceleration option
This adds a setting to control whether hardware acceleration is allowed in built-in panels that use Direct2D.

This is now turned off by default, as the performance seems to be inconsistent for different hardware (and drivers).

foo_ui_columns/d3d_utils.cpp
@@ -2,24 +2,50 @@
 
 namespace cui::d3d {
 
-HRESULT create_d3d_device(D3D_DRIVER_TYPE driver_type, std::span<const D3D_FEATURE_LEVEL> feature_levels,
-    ID3D11Device** device, ID3D11DeviceContext** device_context)
+namespace {
+
+wil::com_ptr<ID3D11Device> create_d3d_device(D3D_DRIVER_TYPE driver_type,
+    std::span<const D3D_FEATURE_LEVEL> feature_levels, ID3D11DeviceContext** device_context = nullptr)
 {
+    wil::com_ptr<ID3D11Device> device;
+
     const auto base_flags = D3D11_CREATE_DEVICE_BGRA_SUPPORT
         | (driver_type == D3D_DRIVER_TYPE_HARDWARE ? D3D11_CREATE_DEVICE_PREVENT_INTERNAL_THREADING_OPTIMIZATIONS : 0);
 #if CUI_ENABLE_D3D_D2D_DEBUG_LAYER == 1
     const auto hr = D3D11CreateDevice(nullptr, driver_type, nullptr, base_flags | D3D11_CREATE_DEVICE_DEBUG,
-        feature_levels.data(), gsl::narrow<uint32_t>(std::size(feature_levels)), D3D11_SDK_VERSION, device, nullptr,
-        device_context);
+        feature_levels.data(), gsl::narrow<uint32_t>(std::size(feature_levels)), D3D11_SDK_VERSION, &d3d_device,
+        nullptr, device_context);
 
-    if (SUCCEEDED(hr) || (hr != DXGI_ERROR_SDK_COMPONENT_MISSING && hr != E_FAIL))
-        return hr;
+    if (SUCCEEDED(hr))
+        return d3d_device;
+
+    if (hr != DXGI_ERROR_SDK_COMPONENT_MISSING && hr != E_FAIL)
+        THROW_HR(hr);
 
     console::print("Columns UI – Direct3D debug layer not installed");
 #endif
 
-    return D3D11CreateDevice(nullptr, driver_type, nullptr, base_flags, feature_levels.data(),
-        gsl::narrow<uint32_t>(std::size(feature_levels)), D3D11_SDK_VERSION, device, nullptr, device_context);
+    THROW_IF_FAILED(D3D11CreateDevice(nullptr, driver_type, nullptr, base_flags, feature_levels.data(),
+        gsl::narrow<uint32_t>(std::size(feature_levels)), D3D11_SDK_VERSION, &device, nullptr, device_context));
+
+    return device;
+}
+
+} // namespace
+
+wil::com_ptr<ID3D11Device> create_d3d_device(
+    const std::span<const D3D_FEATURE_LEVEL> feature_levels, ID3D11DeviceContext** device_context)
+{
+    if (!config::use_hardware_acceleration)
+        return create_d3d_device(D3D_DRIVER_TYPE_WARP, feature_levels);
+
+    try {
+        return create_d3d_device(D3D_DRIVER_TYPE_HARDWARE, feature_levels);
+    } catch (const wil::ResultException&) {
+        const auto device = create_d3d_device(D3D_DRIVER_TYPE_WARP, feature_levels);
+        console::print("Columns UI – failed to create a hardware Direct3D renderer, using a software renderer instead");
+        return device;
+    }
 }
 
 } // namespace cui::d3d

foo_ui_columns/artwork.cpp
@@ -5,6 +5,7 @@
 #include "d2d_utils.h"
 #include "d3d_utils.h"
 #include "imaging.h"
+#include "tab_setup.h"
 
 namespace cui::artwork_panel {
 
@@ -297,10 +298,16 @@ LRESULT ArtworkPanel::on_message(HWND wnd, UINT msg, WPARAM wp, LPARAM lp)
 
         m_display_change_token = system_appearance_manager::add_display_changed_handler(
             [wnd] { PostMessage(wnd, MSG_REFRESH_EFFECTS, 0, 0); });
+
+        m_use_hardware_acceleration_change_token = prefs::add_use_hardware_acceleration_changed_handler([&] {
+            reset_d2d_device_resources();
+            refresh_image();
+        });
         break;
     }
     case WM_DESTROY:
         std::erase(g_windows, this);
+        m_use_hardware_acceleration_change_token.reset();
         m_display_change_token.reset();
         ui_selection_manager::get()->unregister_callback(this);
         playlist_manager_v3::get()->unregister_callback(this);
@@ -504,15 +511,7 @@ void ArtworkPanel::create_d2d_device_resources()
             constexpr std::array feature_levels
                 = {D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_1, D3D_FEATURE_LEVEL_10_0};
 
-            try {
-                THROW_IF_FAILED(d3d::create_d3d_device(
-                    D3D_DRIVER_TYPE_HARDWARE, feature_levels, &m_d3d_device, &m_d3d_device_context));
-            } catch (const wil::ResultException&) {
-                THROW_IF_FAILED(
-                    d3d::create_d3d_device(D3D_DRIVER_TYPE_WARP, feature_levels, &m_d3d_device, &m_d3d_device_context));
-                console::print(
-                    "Artwork view – failed to create a hardware renderer, using a software (WARP) renderer instead");
-            }
+            m_d3d_device = d3d::create_d3d_device(feature_levels, &m_d3d_device_context);
         }
 
         if (!m_d2d_factory)

foo_ui_columns/artwork.h
@@ -205,6 +205,7 @@ private:
     wil::com_ptr<ID2D1Effect> m_scale_effect;
     wil::com_ptr<ID2D1Effect> m_output_effect;
 
+    EventToken::Ptr m_use_hardware_acceleration_change_token;
     EventToken::Ptr m_display_change_token;
     std::shared_ptr<ArtworkReaderManager> m_artwork_reader;
     ArtworkDecoder m_artwork_decoder;

foo_ui_columns/config_vars.cpp
@@ -46,6 +46,9 @@ advconfig_checkbox_factory advbool_close_to_system_tray_icon(
 cfg_bool cfg_playlist_tabs_middle_click(
     {0x3074ea96, 0x22e7, 0x464a, {0x9d, 0xac, 0xec, 0x9, 0xc0, 0xb4, 0x5e, 0x5a}}, true);
 
+fbh::ConfigBool use_hardware_acceleration(
+    {0xd3ef5229, 0x237b, 0x4a90, {0xa8, 0xb8, 0xd6, 0x07, 0x30, 0xdb, 0x7a, 0x20}}, false);
+
 } // namespace cui::config
 
 cfg_int cfg_global(GUID{0xf939e07d, 0x0944, 0x0f40, {0xbf, 0x1f, 0x6d, 0x31, 0xaa, 0x37, 0x10, 0x5f}}, 0);

foo_ui_columns/config_vars.h
@@ -13,6 +13,9 @@ extern advconfig_checkbox_factory advbool_system_tray_icon_x_buttons;
 extern advconfig_checkbox_factory advbool_close_to_system_tray_icon;
 
 extern cfg_bool cfg_playlist_tabs_middle_click;
+
+extern fbh::ConfigBool use_hardware_acceleration;
+
 } // namespace cui::config
 
 class ConfigWindowPlacement : public cfg_var {

foo_ui_columns/d2d_utils.cpp
@@ -1,6 +1,14 @@
 #include "pch.h"
 
+#include "d2d_utils.h"
+
 namespace cui::d2d {
+
+namespace {
+
+std::weak_ptr<wil::com_ptr<ID2D1Factory1>> weak_main_thread_factory;
+
+}
 
 wil::com_ptr<ID2D1Factory1> create_factory(D2D1_FACTORY_TYPE factory_type)
 {
@@ -13,6 +21,17 @@ wil::com_ptr<ID2D1Factory1> create_factory(D2D1_FACTORY_TYPE factory_type)
 
     THROW_IF_FAILED(D2D1CreateFactory(factory_type, options, &factory));
 
+    return factory;
+}
+
+MainThreadD2D1Factory create_main_thread_factory()
+{
+    if (const auto factory = weak_main_thread_factory.lock())
+        return factory;
+
+    const auto factory
+        = std::make_shared<wil::com_ptr<ID2D1Factory1>>(create_factory(D2D1_FACTORY_TYPE_SINGLE_THREADED));
+    weak_main_thread_factory = factory;
     return factory;
 }
 

foo_ui_columns/d2d_utils.h
@@ -4,6 +4,9 @@ namespace cui::d2d {
 
 wil::com_ptr<ID2D1Factory1> create_factory(D2D1_FACTORY_TYPE factory_type);
 
+using MainThreadD2D1Factory = std::shared_ptr<wil::com_ptr<ID2D1Factory1>>;
+MainThreadD2D1Factory create_main_thread_factory();
+
 wil::com_ptr<ID2D1Effect> create_colour_management_effect(const wil::com_ptr<ID2D1DeviceContext>& device_context,
     const wil::com_ptr<ID2D1ColorContext>& source_color_context,
     const wil::com_ptr<ID2D1ColorContext>& dest_color_context);

foo_ui_columns/d3d_utils.h
@@ -2,7 +2,7 @@
 
 namespace cui::d3d {
 
-HRESULT create_d3d_device(D3D_DRIVER_TYPE driver_type, std::span<const D3D_FEATURE_LEVEL> feature_levels,
-    ID3D11Device** device, ID3D11DeviceContext** device_context = nullptr);
+wil::com_ptr<ID3D11Device> create_d3d_device(
+    std::span<const D3D_FEATURE_LEVEL> feature_levels, ID3D11DeviceContext** device_context = nullptr);
 
 }

commit 1d384cf22b4e085b0aa85ec92b3289585e3f2315

Retrieve and set Direct3D device context pointer correctly
This fixes a bug where `cui::d3d::create_d3d_device()` did not pass the `ID3D11DeviceContext**` pointer for the immediate device context on to `uih::d3d::create_d3d_device()` and in turn `D3D11CreateDevice()`.

This would have stopped the Artwork view from calling the `ClearState()` and `Flush()` methods on the device context when releasing or recreating Direct3D and Direct2D resources.

This seems to have regressed in:

https://github.com/reupen/columns_ui/commit/24edec9448d22f3eb486d38a781d2d5acd8ac7e7#diff-5873d652ab9918ca053ca8a9aad53c29ac13e62300d8ad67aef2999156de247d

(I haven’t noticed any problems with the `ClearState()` and `Flush()` calls omitted, but I’m sure they were put in for a reason…)

foo_ui_columns/d3d_utils.cpp
@@ -6,12 +6,12 @@ wil::com_ptr<ID3D11Device> create_d3d_device(
     const std::span<const D3D_FEATURE_LEVEL> feature_levels, ID3D11DeviceContext** device_context)
 {
     if (!config::use_hardware_acceleration)
-        return uih::d3d::create_d3d_device(D3D_DRIVER_TYPE_WARP, feature_levels);
+        return uih::d3d::create_d3d_device(D3D_DRIVER_TYPE_WARP, feature_levels, device_context);
 
     try {
-        return uih::d3d::create_d3d_device(D3D_DRIVER_TYPE_HARDWARE, feature_levels);
+        return uih::d3d::create_d3d_device(D3D_DRIVER_TYPE_HARDWARE, feature_levels, device_context);
     } catch (const wil::ResultException&) {
-        const auto device = uih::d3d::create_d3d_device(D3D_DRIVER_TYPE_WARP, feature_levels);
+        const auto device = uih::d3d::create_d3d_device(D3D_DRIVER_TYPE_WARP, feature_levels, device_context);
         console::print("Columns UI – failed to create a hardware Direct3D renderer, using a software renderer instead");
         return device;
     }